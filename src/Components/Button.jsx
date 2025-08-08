b030f8c0a5c2b20c788c00eb4b2a0d297ffc14d3
Asif Mia <asifmia.dev26@gmail.com>
2025-08-08 21:47:29 +0600

    Add Button components

---
+++ src/Components/Button.jsx
@@ -0,0 +1,27 @@
+
+const type ={
+    primary:[
+        "bg-blue-500",
+        "hover:bg-blue-600",   
+    ],   
+    secondary:[
+        "bg-gray-500", 
+        "hover:bg-gray-600",
+   
+    ],
+    success:[
+        "bg-green-500",    
+        "hover:bg-green-600",
+    ],
+    danger:[
+        "bg-red-500",   
+        "hover:bg-red-600", 
+    ]
+}
+function Button({buttonName , buttonType}){
+    return(
+        <button className= {type[buttonType].join(" ") + "w-2.5 px-6 py-[5px] text-md rounded-md text-white cursor-pointer"}>{buttonName}</button>
+    );
+}
+
+export default Button;
